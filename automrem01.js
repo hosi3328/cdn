831299afa4e5c00c74586bff9406e5c442d16d97
hosi3328 <hosi3328@gmail.com>
2017-08-24 04:43:33 -0300

    Update automrem01.js

--- automrem01.js
+++ automrem01.js
@@ -1,53 +1,50 @@
 function VerificaEstado(){
-    var porta2_estado = document.getElementById("porta2_estado").innerHTML;
-    var porta2_titulo = document.getElementById("porta2_titulo").innerHTML;
+    var portaB_estado = document.getElementById("portaB_estado").innerHTML;
+    var portaB_titulo = document.getElementById("portaB_titulo").innerHTML;
     
-    var porta3_estado = document.getElementById("porta3_estado").innerHTML;
-    var porta3_titulo = document.getElementById("porta3_titulo").innerHTML;
+    var portaC_estado = document.getElementById("portaC_estado").innerHTML;
+    var portaC_titulo = document.getElementById("portaC_titulo").innerHTML;
     
-    var porta4_estado = document.getElementById("porta4_estado").innerHTML;
-    var porta4_titulo = document.getElementById("porta4_titulo").innerHTML;
+    var portaD_estado = document.getElementById("portaD_estado").innerHTML;
+    var portaD_titulo = document.getElementById("portaD_titulo").innerHTML;
     
-    var porta5_estado = document.getElementById("porta5_estado").innerHTML;
-    var porta5_titulo = document.getElementById("porta5_titulo").innerHTML;
+    var portaE_estado = document.getElementById("portaE_estado").innerHTML;
+    var portaE_titulo = document.getElementById("portaE_titulo").innerHTML;
     
-    var porta6_estado = document.getElementById("porta6_estado").innerHTML;
-    var porta6_titulo = document.getElementById("porta6_titulo").innerHTML;
+    var portaF_estado = document.getElementById("portaF_estado").innerHTML;
+    var portaF_titulo = document.getElementById("portaF_titulo").innerHTML;
     
-    var porta7_estado = document.getElementById("porta7_estado").innerHTML;
-    var porta7_titulo = document.getElementById("porta7_titulo").innerHTML;
-
-    var porta8_estado = document.getElementById("porta8_estado").innerHTML;
-    var porta8_titulo = document.getElementById("porta8_titulo").innerHTML;
+    var portaG_estado = document.getElementById("portaG_estado").innerHTML;
+    var portaG_titulo = document.getElementById("portaG_titulo").innerHTML;
     
-    var porta9_estado = document.getElementById("porta9_estado").innerHTML;
-    var porta9_titulo = document.getElementById("porta9_titulo").innerHTML;
+    var portaH_estado = document.getElementById("portaH_estado").innerHTML;
+    var portaH_titulo = document.getElementById("portaH_titulo").innerHTML;
 
-    var porta10_estado = document.getElementById("porta10_estado").innerHTML;
-    var porta10_titulo = document.getElementById("porta10_titulo").innerHTML;
+    var portaI_estado = document.getElementById("portaI_estado").innerHTML;
+    var portaI_titulo = document.getElementById("portaI_titulo").innerHTML;
+    
+    var portaJ_estado = document.getElementById("portaJ_estado").innerHTML;
+    var portaJ_titulo = document.getElementById("portaJ_titulo").innerHTML;
 
-    var porta11_estado = document.getElementById("porta11_estado").innerHTML;
-    var porta11_titulo = document.getElementById("porta11_titulo").innerHTML;
+    var portaK_estado = document.getElementById("portaK_estado").innerHTML;
+    var portaK_titulo = document.getElementById("portaK_titulo").innerHTML;
 
-    var porta12_estado = document.getElementById("porta12_estado").innerHTML;
-    var porta12_titulo = document.getElementById("porta12_titulo").innerHTML;
+    var portaL_estado = document.getElementById("portaL_estado").innerHTML;
+    var portaL_titulo = document.getElementById("portaL_titulo").innerHTML;
 
-    var porta13_estado = document.getElementById("porta13_estado").innerHTML;
-    var porta13_titulo = document.getElementById("porta13_titulo").innerHTML;
+    var portaM_estado = document.getElementById("portaM_estado").innerHTML;
+    var portaM_titulo = document.getElementById("portaM_titulo").innerHTML;
 
-    var porta14_estado = document.getElementById("porta14_estado").innerHTML;
-    var porta14_titulo = document.getElementById("porta14_titulo").innerHTML;
+    var portaN_estado = document.getElementById("portaN_estado").innerHTML;
+    var portaN_titulo = document.getElementById("portaN_titulo").innerHTML;
 
-    var porta15_estado = document.getElementById("porta15_estado").innerHTML;
-    var porta15_titulo = document.getElementById("porta15_titulo").innerHTML;
+    
+ 
 
-    var porta16_estado = document.getElementById("porta16_estado").innerHTML;
-    var porta16_titulo = document.getElementById("porta16_titulo").innerHTML;
-
-    if(porta2_estado === "0"){
-        document.getElementById("porta2_botao").innerHTML="<div class='porta_desligada'></div><a href='/?l2' class='botao'>"+porta2_titulo+"</a>";
+    if(porta'B'_estado === "0"){
+        document.getElementById("portaB_botao").innerHTML="<div class='porta_desligada'></div><a href='/?lB' class='botao'>"+porta'B'_titulo+"</a>";
     } else {
-        document.getElementById("porta2_botao").innerHTML="<div class='porta_ligada'></div><a href='/?d2' class='botao'>"+porta2_titulo+"</a>";
+        document.getElementById("portaB_botao").innerHTML="<div class='porta_ligada'></div><a href='/?dB' class='botao'>"+porta'B'_titulo+"</a>";
     }
 
     if(porta3_estado === "0"){
